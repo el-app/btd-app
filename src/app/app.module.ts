1657c59e3c2d57f884d08608258f8f11ea6ce636
elapp <elise.martin@me.com>
2021-12-11 16:03:47 +0100

    Adding structure with components, services and guards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ShopComponent } from './shop/shop.component';
+import { ProductsComponent } from './shop/products/products.component';
+import { ProductComponent } from './shop/product/product.component';
+import { CartComponent } from './shop/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactComponent,
+    LoginComponent,
+    RegisterComponent,
+    ShopComponent,
+    ProductsComponent,
+    ProductComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
