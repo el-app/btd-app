acdcc7017b0b6f368a6c5b5be7cd2aeed22f633c
Élise Martin <elise.martin@me.com>
2022-01-07 09:45:44 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,5 @@ const BASE_URL = 'http://localhost:8888/el-btd-app/btd-backend/';
 export const environment = {
   production: false,
   API: BASE_URL+'api/',
-  API_KEY: "API_KEY=emgc2324ecsc-2008-a11y-me87-mayaBee42cdp"
+  API_KEY: "API_KEY=XXXXXXX"
 };
